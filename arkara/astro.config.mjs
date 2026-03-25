be3355be136904f4d4146e21eee6a0024fb66774
Gleand Latuni <estetikaglend@gmail.com>
2026-03-25 15:29:55 +0900

    fix SEO, sitemap google optimize

--- arkara/astro.config.mjs
+++ arkara/astro.config.mjs
@@ -1,23 +1,26 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   site: 'https://arkaraweb.com',
 
+  adapter: node({
+    mode: 'standalone'
+  }),
+
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [mdx()],
-
-  adapter: node({
-    mode: 'standalone'
-  }),
+  integrations: [
+    mdx(),
+    sitemap(),
+  ],
 
   image: {
     domains: [
