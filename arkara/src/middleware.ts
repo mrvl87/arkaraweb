f13e4ceec169184f3ea1290f8076acf34d5c51dc
Gleand Latuni <estetikaglend@gmail.com>
2026-06-18 11:51:55 +0900

    Add legacy redirects for Bing scan URLs

--- arkara/src/middleware.ts
+++ arkara/src/middleware.ts
@@ -1,5 +1,14 @@
 import { defineMiddleware } from 'astro:middleware';
 
+const LEGACY_PATH_REDIRECTS = new Map<string, string>([
+  ['/blog/simulasi-3-hari-tanpa-listrik', '/blog/simulasi-3-hari-tanpa-listrik-di-rumah-anda'],
+  ['/blog/paradoks-mi-instan', '/blog/paradoks-mi-instan-ketahanan-dan-pangan'],
+  [
+    '/blog/5-metode-mengolah-air-kotor',
+    '/panduan/5-metode-mengolah-air-kotor-jadi-layak-minum-perbandingan-lengkap',
+  ],
+]);
+
 export const onRequest = defineMiddleware(async (context, next) => {
   const url = new URL(context.request.url);
   const canonicalHost = 'arkaraweb.com';
@@ -11,6 +20,13 @@ export const onRequest = defineMiddleware(async (context, next) => {
     url.protocol = 'https:';
     url.hostname = canonicalHost;
     return context.redirect(url.href, 301);
+  }
+
+  const normalizedPath = url.pathname.replace(/\/+$/, '') || '/';
+  const legacyTarget = LEGACY_PATH_REDIRECTS.get(normalizedPath);
+
+  if (legacyTarget) {
+    return context.redirect(new URL(legacyTarget, `https://${canonicalHost}`).href, 301);
   }
 
   return next();
